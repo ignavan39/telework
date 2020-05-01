824ce6f8a60791dca5f8e14a245a00ed27033cf6
Иван Игнатенко <ivanignatenko28@gmail.com>
2020-05-01 17:30:22 +0600

    Back ready

---
+++ back.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const csv = require('csv-parser');
+const results = [];
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080'); // update to match the domain you will make the request from
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+fs.createReadStream('table.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        console.log(results);
+    });
+app.get('/', (req, res, next) => {
+    res.send(results);
+});
+app.listen(PORT, () => {
+    console.log(`Server startred on port ${PORT}`);
+});
---
+++ client/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      teachers:[],
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000').then(
+      (response)=> {
+        console.log(response.data);
+        this.teachers = response.data;
+      }
+    ).catch((e)=>{
+      console.error(e);
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
