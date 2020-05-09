cbd739883e4df64908b851713bc9c033b4d075b1
Иван Игнатенко <ivanignatenko28@gmail.com>
2020-05-09 12:26:26 +0600

    Kalachinsk update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+     <div v-if="!loader">
+      <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+    
     <div v-if="teachers">
       <StatusSchool :optionSet="areaSet" :teachers="teachers" :choice="'area'"></StatusSchool>
       <hr />
 
       <StatusSchool :optionSet="schoolSet" :teachers="teachers" :choice="'school'"></StatusSchool>
       <!--  <TimeOnJob :teachers="teachers"></TimeOnJob>-->
+    
     </div>
   </div>
 </template>
@@ -24,10 +29,12 @@ export default {
   data: () => ({
     teachers: [],
     schoolSet: {},
-    areaSet: {}
+    areaSet: {},
+    loader: false,
   }),
   async mounted() {
-    await axios
+   await 
+      axios
       .get("/api")
       .then(response => {
         console.log(response.data);
@@ -38,6 +45,8 @@ export default {
           this.schoolSet.add(item.school.trim());
           this.areaSet.add(item.Area.trim());
         }
+        
+        this.loader = true;
       })
       .catch(e => {
         console.error(e);
@@ -47,6 +56,90 @@ export default {
 </script>
 
 <style>
+.lds-roller {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-roller div {
+  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  transform-origin: 40px 40px;
+}
+.lds-roller div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: #dfc;
+  margin: -4px 0 0 -4px;
+}
+.lds-roller div:nth-child(1) {
+  animation-delay: -0.036s;
+}
+.lds-roller div:nth-child(1):after {
+  top: 63px;
+  left: 63px;
+}
+.lds-roller div:nth-child(2) {
+  animation-delay: -0.072s;
+}
+.lds-roller div:nth-child(2):after {
+  top: 68px;
+  left: 56px;
+}
+.lds-roller div:nth-child(3) {
+  animation-delay: -0.108s;
+}
+.lds-roller div:nth-child(3):after {
+  top: 71px;
+  left: 48px;
+}
+.lds-roller div:nth-child(4) {
+  animation-delay: -0.144s;
+}
+.lds-roller div:nth-child(4):after {
+  top: 72px;
+  left: 40px;
+}
+.lds-roller div:nth-child(5) {
+  animation-delay: -0.18s;
+}
+.lds-roller div:nth-child(5):after {
+  top: 71px;
+  left: 32px;
+}
+.lds-roller div:nth-child(6) {
+  animation-delay: -0.216s;
+}
+.lds-roller div:nth-child(6):after {
+  top: 68px;
+  left: 24px;
+}
+.lds-roller div:nth-child(7) {
+  animation-delay: -0.252s;
+}
+.lds-roller div:nth-child(7):after {
+  top: 63px;
+  left: 17px;
+}
+.lds-roller div:nth-child(8) {
+  animation-delay: -0.288s;
+}
+.lds-roller div:nth-child(8):after {
+  top: 56px;
+  left: 12px;
+}
+@keyframes lds-roller {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 #app {
   width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
