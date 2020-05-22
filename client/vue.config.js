3d78d3047c8c504fff64660248b483244334c153
Иван Игнатенко <ivanignatenko28@gmail.com>
2020-05-22 18:10:58 +0600

    fix style ready

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../server/public'),
+    outputDir: path.resolve(__dirname, '../public'),
     devServer: {
         proxy: {
             '/api': {
