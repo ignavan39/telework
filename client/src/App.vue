0a7d59b13c125ea1f88b31a1e45ca48097568c0d
Dmitriy Shelgunov <shelgunovdima3@gmail.com>
2020-10-17 15:13:46 +0600

    add default vue ts config

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,63 +1,24 @@
 <template>
-  <div id="app">
-    <intro />
-  
-  
-    <div v-if="!getLoader">
-      <div class="progress">
-        <div class="indeterminate"></div>
-      </div>
-    </div>
-    <div class="container">
-      <div v-if="dumpTeachers">
-        <Pie :optionSet="getAreaSet" :teachers="dumpTeachers" :optionName="'район'"></Pie>
-      
-        <Chart :optionSet="getSchoolSet" :teachers="dumpTeachers" :choice="'school'" :optionName="'школу'"></Chart>
-        <Readiness :optionSet="getSchoolSet" :teachers="dumpTeachers" :optionName="'школу'"></Readiness>
-       
-      </div>
-    </div>
-    <Footer />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
 </template>
 
-<script>
-import Footer from "../src/components/Footer";
-import Intro from "../src/components/Intro";
-import Chart from "../src/components/Chart";
-import Pie from "../src/components/Pie";
-import Readiness from '../src/components/Readiness'
-//import TimeOnTelephone from '../src/components/TimeOnTelephone'
-import { mapGetters, mapActions } from "vuex";
-export default {
-  name: "App",
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
 
+@Options({
   components: {
-    Chart,
-    Footer,
-    Intro,
-    Pie,
-    Readiness,
-   // TimeOnTelephone
   },
-  methods: mapActions(["fetchTeachers"]),
-  computed: mapGetters([
-    "dumpTeachers",
-    "getAreaSet",
-    "getSchoolSet",
-    "getLoader"
-  ]),
-  async mounted() {
-    this.fetchTeachers();
-  }
-};
+})
+export default class App extends Vue {}
 </script>
+
 <style>
-*{
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-
 </style>
-
-
