34e1c99944efd9832592bafa997b33620acb4707
kitt3911 <ivanignatenko28@gmail.com>
2020-11-07 10:05:17 +0600

    add middleware in redux

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,4 +1,8 @@
-import {createStore} from 'redux'
+import {applyMiddleware, compose, createStore} from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 import { answersReducer } from './answersReducer'
 
-export const store = createStore(answersReducer)
+export const store = createStore(answersReducer,compose(
+    applyMiddleware(logger,thunk)
+))
