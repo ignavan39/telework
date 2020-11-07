ef259f2d9b8d2ef7d244cdd2964c688b8aee27c7
kitt3911 <ivanignatenko28@gmail.com>
2020-11-07 22:17:13 +0600

    production build

--- back.js
+++ back.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const csv = require('csv-parser');
 const results = [];
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header(
         'Access-Control-Allow-Headers',
@@ -24,15 +24,8 @@ app.get('/api', (req, res, next) => {
     res.send(results);
 });
 
-if (process.env.NODE_ENV === 'production') {
-    //static
-    app.use(express.static(__dirname + '/public/'));
-
-    //SPA
-
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
-
+app.use(express.static(__dirname + '/client/build'));
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/client/build/index.html'));
 app.listen(PORT, () => {
     console.log(`Server startred on port ${PORT}`);
 });
